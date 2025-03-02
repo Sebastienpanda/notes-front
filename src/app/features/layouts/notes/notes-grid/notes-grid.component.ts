29cab04284869df925b657ac4dfb6b9f82d9eb46
Sebastienpanda <sebastienweb27@gmail.com>
2025-03-02 20:41:36 +0100

    Update layout if empty array notes

--- src/app/features/layouts/notes/notes-grid/notes-grid.component.ts
+++ src/app/features/layouts/notes/notes-grid/notes-grid.component.ts
@@ -3,12 +3,13 @@ import { Component, OnInit } from '@angular/core';
 import { Note, NoteService } from '@core/services/note.service';
 import { NotesHeaderComponent } from '../header/header.component';
 import { NotesListComponent } from '../notes-list/notes-list.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
 
 @Component({
     selector: 'app-notes-grid',
     templateUrl: './notes-grid.component.html',
     styleUrls: ['./notes-grid.component.scss'],
-    imports: [CommonModule, NotesHeaderComponent, NotesListComponent],
+    imports: [CommonModule, NotesHeaderComponent, NotesListComponent, SidebarComponent],
 })
 export class NotesGridComponent implements OnInit {
     constructor(private noteService: NoteService) {}
