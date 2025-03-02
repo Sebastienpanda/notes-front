368c7628f5ac4642981923b35dfe16d7139c9dd9
Sebastienpanda <sebastienweb27@gmail.com>
2025-03-02 15:58:30 +0100

    Add Header layout notes

---
+++ src/app/features/layouts/notes/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-notes-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss'],
+})
+export class NotesHeaderComponent {}
