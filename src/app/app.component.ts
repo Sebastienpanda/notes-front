d50ad0956a9579eba3d3a38bec53127913538df5
Sebastienpanda <sebastienweb27@gmail.com>
2025-03-02 15:57:31 +0100

    Add logics display navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { NavigationComponent } from '@features/layouts/navigation/navigation.component';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, CommonModule, NavigationComponent],
     templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+    isMobileOrTablet = signal(false);
+    hideNavigation = signal(false);
+
+    constructor(
+        private breakpointObserver: BreakpointObserver,
+        private router: Router,
+        private route: ActivatedRoute
+    ) {
+        this.breakpointObserver.observe([Breakpoints.Handset, Breakpoints.Tablet]).subscribe(result => {
+            this.isMobileOrTablet.set(result.matches);
+        });
+    }
+
+    ngOnInit() {
+        this.router.events.subscribe(() => {
+            this.hideNavigation.set(this.router.url.includes('/connect'));
+        });
+    }
+}
