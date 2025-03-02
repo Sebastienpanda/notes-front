24c9c0ce5744d32cb52e6dd2f3747fe0c4cbc3e5
Sebastienpanda <sebastienweb27@gmail.com>
2025-03-02 15:58:44 +0100

    Add logics grid layout notes

---
+++ src/app/features/layouts/notes/notes-grid/notes-grid.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { Note, NoteService } from '@core/services/note.service';
+import { NotesHeaderComponent } from '../header/header.component';
+import { NotesListComponent } from '../notes-list/notes-list.component';
+
+@Component({
+    selector: 'app-notes-grid',
+    templateUrl: './notes-grid.component.html',
+    styleUrls: ['./notes-grid.component.scss'],
+    imports: [CommonModule, NotesHeaderComponent, NotesListComponent],
+})
+export class NotesGridComponent implements OnInit {
+    constructor(private noteService: NoteService) {}
+    notes: Note[] = [];
+
+    ngOnInit() {
+        this.noteService.getNotes().subscribe(data => {
+            this.notes = data;
+        });
+    }
+
+    trackById(index: number, note: { id: number }): number {
+        return note.id;
+    }
+}
